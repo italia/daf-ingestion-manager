b2167c5a919edb552080db6956c7cc16a78628cf
Alberto Massidda <albertomassidda@aol.com>
2018-01-30 19:38:49 +0100

    moved image name in deployment script testing rolling-update behaviour

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -9,6 +9,8 @@ set -e
 
 export INGESTION_MANAGER_VERSION=1.0.0-SNAPSHOT
 
+export IMAGE_URI=registry.daf.teamdigitale.it/daf-ingestion-manager:${INGESTION_MANAGER_VERSION}
+
 # test for template
 envsubst < daf_ingestion_manager.yml > output.yml
 
